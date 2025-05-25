2f5a313df21adc9c9b8bb96bca65d3d607df7c6f
Mina Asaad <minamarkasaad10@gmail.com>
2025-05-25 14:54:11 +0300

    fix

--- src/socket.jsx
+++ src/socket.jsx
@@ -10,11 +10,6 @@ const API_URL = (
 ).replace(/\/$/, "");
 
 export const socket = io(API_URL, {
-  withCredentials: true,
-  transports: ["websocket"],
-  upgrade: true,
-  rememberUpgrade: true,
-  timeout: 20000,
-  forceNew: true,
-  secure: true
+  withCredentials: true, // if you’re sending cookies or need auth
+  transports: ["websocket"], // skip polling altogether
 });
